fbe6192e8562d5b4803c5cb6883726a30d285e22
Eduardo Knopp <eduardoknopp7@gmail.com>
2024-05-22 11:41:25 -0300

    feat: add protection to routes using authcontext

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,20 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import LoginPage from "./pages/login";
+import { useAuth } from "./context/AuthContext";
+import NotFoundPage from "./pages/notFound";
 
 const App: React.FC = () => {
+  const { isAuthenticated } = useAuth();
   return (
     <Router>
       <div>
         <Routes>
-          <Route path="/" element={<LoginPage />} />
+          <Route path="/login" element={<LoginPage />} />
+          {isAuthenticated ? (
+            <Route path="/home" element={<LoginPage />} />
+          ) : null}
+          <Route path="*" element={<NotFoundPage />} />
         </Routes>
       </div>
     </Router>
--- frontend/src/pages/login/index.tsx
+++ frontend/src/pages/login/index.tsx
@@ -1,21 +1,26 @@
+// src/pages/login/index.tsx
 import React, { useState } from "react";
+import { useAuth } from "../../context/AuthContext";
 import Input from "../../components/common/Input";
 import Button from "../../components/common/Button";
 
 const Login = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const { login } = useAuth();
 
   const handleLogin = (e: React.FormEvent) => {
     e.preventDefault();
-    // Adicione a lógica de login aqui
+    // Adicione aqui a lógica para chamar a API de autenticação e obter o token
+    const token = "fake-token"; // Substitua pelo token real obtido da API
+    login(token);
   };
 
   return (
     <div className="flex items-center justify-center min-h-screen bg-gray-100">
-      <div className="bg-white p-8 rounded-lg shadow-md w-96">
+      <div className="bg-white p-8 rounded-lg shadow-md w-120">
         <div className="flex justify-center mb-6">
-          <img src="/path-to-your-logo.png" alt="Logo" className="h-20" />
+          <img src="/Kanban-Board.png" alt="Logo" className="h-20" />
         </div>
         <form onSubmit={handleLogin}>
           <Input
@@ -35,7 +40,7 @@ const Login = () => {
           <div className="flex justify-between items-center mb-4">
             <Button type="submit">Login</Button>
             <Button type="button" onClick={() => alert("Esqueceu a senha?")}>
-              Esquecer a Senha
+              Esqueceu a senha?
             </Button>
           </div>
           <div className="flex justify-center">
