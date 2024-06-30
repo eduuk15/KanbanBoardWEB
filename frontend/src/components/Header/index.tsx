20f5192f3b3a2e8bbf5e07206dc1f09a4c0af1b3
Igor Ferreira <igorferreirafonseca@gmail.com>
2024-06-30 20:00:07 -0300

    correções

--- frontend/src/components/Header/index.tsx
+++ frontend/src/components/Header/index.tsx
@@ -1,5 +1,5 @@
 import { FaUserAlt } from "react-icons/fa";
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { useAuth } from "../../context/AuthContext";
 
--- frontend/src/services/axiosInstance.ts
+++ frontend/src/services/axiosInstance.ts
@@ -1,4 +1,4 @@
-import axios, { AxiosInstance, AxiosError } from "axios";
+import axios, { AxiosInstance } from "axios";
 
 export const axiosInstance: AxiosInstance = axios.create({
   baseURL: "http://localhost:8000",
