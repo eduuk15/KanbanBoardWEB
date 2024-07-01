9c9ea9e42225c0e752a92a549d2953d0dbac7148
Eduardo Knopp <eduardoknopp7@gmail.com>
2024-07-01 12:59:20 -0300

    jajaj

--- frontend/src/services/axiosInstance.ts
+++ frontend/src/services/axiosInstance.ts
@@ -1,7 +1,7 @@
 import axios, { AxiosInstance } from "axios";
 
 export const axiosInstance: AxiosInstance = axios.create({
-  baseURL: "https://kanbanboardweb.railway.internal:8000",
+  baseURL: "https://kanbanboardweb.railway.internal:8000/",
 });
 
 axiosInstance.interceptors.request.use(
