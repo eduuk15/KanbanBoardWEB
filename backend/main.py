8e85adfdb6ed161cfae3c1c168f2db65ed068781
Eduardo Knopp <eduardoknopp7@gmail.com>
2024-07-01 09:41:21 -0300

    test middleware

--- backend/main.py
+++ backend/main.py
@@ -21,12 +21,13 @@ origins = [
 
 app.add_middleware(
     CORSMiddleware,
-    HTTPSRedirectMiddleware,
     allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],   
 )
+
+app.add_middleware(HTTPSRedirectMiddleware)
 
 from api.models.task import Task
 from api.models.user import User
