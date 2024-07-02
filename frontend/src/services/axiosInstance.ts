750208d9c04744790580d72bcb2c0307dff2c4bc
Eduardo Knopp <eduardoknopp7@gmail.com>
2024-07-02 16:03:35 -0300

    deploy9

--- frontend/src/services/axiosInstance.ts
+++ frontend/src/services/axiosInstance.ts
@@ -4,28 +4,28 @@ export const axiosInstance: AxiosInstance = axios.create({
   baseURL: "https://kanbanboardweb-production-e73b.up.railway.app",
 });
 
-axiosInstance.interceptors.request.use(
-  (config) => {
-    const token = localStorage.getItem("token");
-    if (token) {
-      config.headers.Authorization = `Bearer ${token}`;
-    }
-    return config;
-  },
-  (error) => {
-    return Promise.reject(error);
-  }
-);
+// axiosInstance.interceptors.request.use(
+//   (config) => {
+//     const token = localStorage.getItem("token");
+//     if (token) {
+//       config.headers.Authorization = `Bearer ${token}`;
+//     }
+//     return config;
+//   },
+//   (error) => {
+//     return Promise.reject(error);
+//   }
+// );
 
-axiosInstance.interceptors.response.use(
-  (response) => {
-    return response;
-  },
-  (error) => {
-    handleAxiosError(error);
-    return Promise.reject(error);
-  }
-);
+// axiosInstance.interceptors.response.use(
+//   (response) => {
+//     return response;
+//   },
+//   (error) => {
+//     handleAxiosError(error);
+//     return Promise.reject(error);
+//   }
+// );
 
 const handleAxiosError = (error: any) => {
   if (error.response) {
