95933ad9e1f8691f9f64bd33bbf345372d72a69a
Eduardo Knopp <eduardoknopp7@gmail.com>
2024-07-02 16:14:40 -0300

    deploy11

--- frontend/src/api/login.ts
+++ frontend/src/api/login.ts
@@ -2,10 +2,13 @@ import { axiosInstance } from "../services/axiosInstance";
 
 export const loginUser = async (email: string, password: string) => {
   try {
-    const response = await axiosInstance.post("/login", {
-      email,
-      password,
-    });
+    const response = await axiosInstance.post(
+      "https://kanbanboardweb-production-e73b.up.railway.app/login",
+      {
+        email,
+        password,
+      }
+    );
     return response.data;
   } catch (error) {
     throw error;
--- frontend/src/services/axiosInstance.ts
+++ frontend/src/services/axiosInstance.ts
@@ -4,36 +4,36 @@ export const axiosInstance: AxiosInstance = axios.create({
   baseURL: "https://kanbanboardweb-production-e73b.up.railway.app",
 });
 
-// axiosInstance.interceptors.request.use(
-//   (config) => {
-//     const token = localStorage.getItem("token");
-//     if (token) {
-//       config.headers.Authorization = `Bearer ${token}`;
-//     }
-//     return config;
-//   },
-//   (error) => {
-//     return Promise.reject(error);
-//   }
-// );
+axiosInstance.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("token");
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
 
-// axiosInstance.interceptors.response.use(
-//   (response) => {
-//     return response;
-//   },
-//   (error) => {
-//     handleAxiosError(error);
-//     return Promise.reject(error);
-//   }
-// );
+axiosInstance.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  (error) => {
+    handleAxiosError(error);
+    return Promise.reject(error);
+  }
+);
 
-// const handleAxiosError = (error: any) => {
-//   if (error.response) {
-//     console.error("Request failed with status:", error.response.status);
-//     console.error("Error details:", error.response.data);
-//   } else if (error.request) {
-//     console.error("Request failed:", error.request);
-//   } else {
-//     console.error("Error:", error.message);
-//   }
-// };
+const handleAxiosError = (error: any) => {
+  if (error.response) {
+    console.error("Request failed with status:", error.response.status);
+    console.error("Error details:", error.response.data);
+  } else if (error.request) {
+    console.error("Request failed:", error.request);
+  } else {
+    console.error("Error:", error.message);
+  }
+};
